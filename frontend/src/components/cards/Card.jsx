d6a60f4d34a0e43418ba5d5e5bf9588a06c16542
GosseFlorian <floriangosse91@gmail.com>
2026-05-19 15:28:55 +0200

    Mise à jour du composents  suite à l'ajout d'un hook personnalisé useAppStore et création d'un fichier css pour stylisé plus tard

--- frontend/src/components/cards/Card.jsx
+++ frontend/src/components/cards/Card.jsx
@@ -1,10 +1,19 @@
 import ProgressBar from "@ramonak/react-progress-bar";
+import { SkillsList } from "../skills-list/SkillsList";
+import { useAppStore } from "../../../store/useAppStore";
+import "./Card.css";
 
 export function Card({ theme }) {
+  const getProgressForTheme = useAppStore((s) => s.getProgressForTheme);
+  const progress = getProgressForTheme(theme.id);
+
   return (
     <>
-      <h2>{theme}</h2>
-      <ProgressBar completed={60} />
+      <div className="card">
+        <h2>{theme.name}</h2>
+        <ProgressBar labelAlignment="left" completed={progress} />
+        <SkillsList themeId={theme.id} />
+      </div>
     </>
   );
 }
