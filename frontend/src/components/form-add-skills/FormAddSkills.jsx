0e364213e71d2bbdbf44e5d117ab793a14b002f4
GosseFlorian <floriangosse91@gmail.com>
2026-05-19 15:36:20 +0200

    création du formulaire d'ajout de skill. TODO: ajouter un fichier style

---
+++ frontend/src/components/form-add-skills/FormAddSkills.jsx
@@ -0,0 +1,53 @@
+import { useAppStore } from "../../../store/useAppStore";
+import { useState } from "react";
+
+export function FormAddSkills({ themeId, onClose }) {
+  const { addSkill } = useAppStore();
+
+  const [description, setDescription] = useState("");
+  const [isDone, setIsDone] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!description.trim()) return;
+    await addSkill(description, themeId, isDone);
+    setDescription("");
+    setIsDone(false);
+    onClose();
+  };
+  return (
+    <div className="form-container">
+      <form onSubmit={handleSubmit}>
+        <p>Ajouter une compétence</p>
+        <input
+          type="text"
+          required
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <p>La compétence est :</p>
+        <label>
+          <input
+            type="radio"
+            name="isDone"
+            value="true"
+            checked={isDone === true}
+            onChange={() => setIsDone(true)}
+          />
+          Validée
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="isDone"
+            value="false"
+            checked={isDone === false}
+            onChange={() => setIsDone(false)}
+          />
+          Pas validé
+        </label>
+        <button type="submit">Ajouter</button>
+      </form>
+    </div>
+  );
+}
