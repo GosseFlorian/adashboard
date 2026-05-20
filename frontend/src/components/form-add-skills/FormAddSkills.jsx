e13e42283a28bb4fde7f20efd0aba92e82f13d51
GosseFlorian <floriangosse91@gmail.com>
2026-05-20 11:17:55 +0200

    suppression de la div car non utile

--- frontend/src/components/form-add-skills/FormAddSkills.jsx
+++ frontend/src/components/form-add-skills/FormAddSkills.jsx
@@ -1,5 +1,6 @@
 import { useAppStore } from "../../../store/useAppStore";
 import { useState } from "react";
+import "./FormAddSkills.css";
 
 export function FormAddSkills({ themeId, onClose }) {
   const { addSkill } = useAppStore();
@@ -16,38 +17,36 @@ export function FormAddSkills({ themeId, onClose }) {
     onClose();
   };
   return (
-    <div className="form-container">
-      <form onSubmit={handleSubmit}>
-        <p>Ajouter une compétence</p>
+    <form onSubmit={handleSubmit} className="form-container">
+      <p>Ajouter une compétence</p>
+      <input
+        type="text"
+        required
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <p>La compétence est :</p>
+      <label>
         <input
-          type="text"
-          required
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
+          type="radio"
+          name="isDone"
+          value="true"
+          checked={isDone === true}
+          onChange={() => setIsDone(true)}
         />
-        <p>La compétence est :</p>
-        <label>
-          <input
-            type="radio"
-            name="isDone"
-            value="true"
-            checked={isDone === true}
-            onChange={() => setIsDone(true)}
-          />
-          Validée
-        </label>
-        <label>
-          <input
-            type="radio"
-            name="isDone"
-            value="false"
-            checked={isDone === false}
-            onChange={() => setIsDone(false)}
-          />
-          Pas validé
-        </label>
-        <button type="submit">Ajouter</button>
-      </form>
-    </div>
+        Validée
+      </label>
+      <label>
+        <input
+          type="radio"
+          name="isDone"
+          value="false"
+          checked={isDone === false}
+          onChange={() => setIsDone(false)}
+        />
+        Pas validé
+      </label>
+      <button type="submit">Ajouter</button>
+    </form>
   );
 }
