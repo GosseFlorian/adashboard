aac70b4a70fa1652cf2b0c5b4e46e16b84f28d66
GosseFlorian <floriangosse91@gmail.com>
2026-05-20 11:58:10 +0200

    ajout d'une div overlay et popup pour ouvrir le formulaire en popup

--- frontend/src/components/form-add-skills/FormAddSkills.jsx
+++ frontend/src/components/form-add-skills/FormAddSkills.jsx
@@ -2,9 +2,9 @@ import { useAppStore } from "../../../store/useAppStore";
 import { useState } from "react";
 import "./FormAddSkills.css";
 
-export function FormAddSkills({ themeId, onClose }) {
-  const { addSkill } = useAppStore();
-
+export function FormAddSkills({ themeId }) {
+  const addSkill = useAppStore((s) => s.addSkill);
+  const closeForm = useAppStore((s) => s.closeForm);
   const [description, setDescription] = useState("");
   const [isDone, setIsDone] = useState(false);
 
@@ -14,39 +14,50 @@ export function FormAddSkills({ themeId, onClose }) {
     await addSkill(description, themeId, isDone);
     setDescription("");
     setIsDone(false);
-    onClose();
+    closeForm();
   };
+
   return (
-    <form onSubmit={handleSubmit} className="form-container">
-      <p>Ajouter une compétence</p>
-      <input
-        type="text"
-        required
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-      />
-      <p>La compétence est :</p>
-      <label>
-        <input
-          type="radio"
-          name="isDone"
-          value="true"
-          checked={isDone === true}
-          onChange={() => setIsDone(true)}
-        />
-        Validée
-      </label>
-      <label>
-        <input
-          type="radio"
-          name="isDone"
-          value="false"
-          checked={isDone === false}
-          onChange={() => setIsDone(false)}
-        />
-        Pas validé
-      </label>
-      <button type="submit">Ajouter</button>
-    </form>
+    <div className="overlay" onClick={closeForm}>
+      <div className="popup" onClick={(e) => e.stopPropagation()}>
+        <h3>Ajouter une compétence</h3>
+
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            required
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            placeholder="Je sais..."
+          />
+
+          <div className="radio-group">
+            <p>La compétence est :</p>
+            <label>
+              <input
+                type="radio"
+                name="isDone"
+                checked={isDone === true}
+                onChange={() => setIsDone(true)}
+              />
+              Validée
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="isDone"
+                checked={isDone === false}
+                onChange={() => setIsDone(false)}
+              />
+              Pas validée
+            </label>
+          </div>
+
+          <div className="form-actions">
+            <button type="submit">Ajouter</button>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 }
