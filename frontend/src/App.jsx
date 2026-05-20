a9c8003db9f5ffffd5ccb57267bf9046a8c7d9aa
GosseFlorian <floriangosse91@gmail.com>
2026-05-20 11:29:12 +0200

    ajout de l'affichage du formulaire à la racine

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 // import { useState } from 'react'
 import "./App.css";
 import { CardList } from "./components/card-list/CardList";
+import { FormAddSkills } from "./components/form-add-skills/FormAddSkills";
 import { useAppStore } from "../store/useAppStore";
 import { useEffect } from "react";
 
 function App() {
   const loadData = useAppStore((s) => s.loadData);
+  const showFormForTheme = useAppStore((s) => s.showFormForTheme);
 
   useEffect(() => {
     loadData();
@@ -15,6 +17,9 @@ function App() {
     <>
       <h1>Hello</h1>
       <CardList />
+      {showFormForTheme !== null && (
+        <FormAddSkills themeId={showFormForTheme} />
+      )}
     </>
   );
 }
